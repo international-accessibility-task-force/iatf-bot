1b4dd9bd03e8e319e759bf23257c2fc3be4d1d13
mugiwarafx <sangalesbenes@uoc.edu>
2022-09-12 13:41:21 +0200

    updates server

--- server.js
+++ server.js
@@ -2,7 +2,15 @@ const express = require('express')
 const server = express()
 
 server.all('/', (req, res) => {
-  res.send(`OK`)
+  res.send('serving keepalive with express')
+})
+
+server.get('/healthcheck', (req, res) => {
+  res.send('ok')
+})
+
+server.get('/version', (req, res) => {
+  res.send('0.0.1')
 })
 
 function keepAlive() {
