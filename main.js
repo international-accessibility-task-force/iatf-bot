f8da8af61ec53254a42fb67ed798febcb718fd81
mugiwarafx <sangalesbenes@uoc.edu>
2022-09-18 18:57:57 +0200

    fix await

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ client.once('ready', () => {
 client.on('messageCreate', async (msg) => {
   const logs = await client.channels.cache.get('1018843354705956956')
   const channelMessages = await msg.channel.messages.channel.messages.fetch({ limit: 100 })
-  const authorRoles = msg.member.roles.cache.map((role) => role.name)
+  const authorRoles = await msg.member.roles.cache.map((role) => role.name)
   const bot = new Bot(msg, msg.guild, msg.channel, msg.author, authorRoles, msg.content, channelMessages, client, logs)
 
   await bot.channelClear()
