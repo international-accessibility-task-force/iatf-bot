ae71b14042d8e915dff30f745759743ea9c948d9
mugiwarafx <sangalesbenes@uoc.edu>
2022-10-05 20:32:44 +0200

    simplify main.ts

--- lib/types/interfaces.ts
+++ lib/types/interfaces.ts
@@ -1,14 +1,8 @@
 import { Message, TextChannel } from 'discord.js'
 
-//TODO: map discord.js types where any
 export interface BotClass {
   message: Message
-  messageChannel: any
-  messageAuthor: any
-  messageAuthorRoles: any
-  messageContent: any
-  channelMessages: any
-  logsChannel: TextChannel
+  logChannel: TextChannel
 }
 
 export interface Channels {
